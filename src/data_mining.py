07778d85185892f495bbe5e8e332548f63aa3280
Evan <evancjx@gmail.com>
2020-06-20 13:35:03 +0800

    Update 200620 (1) OOP functions New text preprocessing logic

--- src/data_mining.py
+++ src/data_mining.py
@@ -1,42 +1,65 @@
 
 from .helper import datetime_convert_timezone, datetime_string_to_datetime 
-from .text_preprocessing import clean_tweet, text_sentiment
+from .text_preprocessing import TP
 
-tweet_created_at_format = '%a %b %d %H:%M:%S %z %Y'
-to_tz = 'Asia/Singapore'
+class DM(TP):
+    tweet_created_at_format = '%a %b %d %H:%M:%S %z %Y'
+    to_tz = 'Asia/Singapore'
 
-def organize_tweet(tweet_json):
-    text = tweet_json.get('text')
-    urls = [
-        url.get('url')
-        for url in tweet_json.get('entities', {'urls': []}).get('urls', [])+
-        tweet_json.get('entities', {'media': []}).get('media', [])+
-        tweet_json.get('extended_entities', {'media': []}).get('media', [])
-    ]
-    text_cleaned = clean_tweet(text, urls)
-    polarity, subjectivity = text_sentiment(text_cleaned)
+    def __init__(self, tokenizer=None, stop_words=()):
+        super().__init__(stop_words=stop_words)
 
-    return {
-        'id': tweet_json.get('id_str'),
-        'created_at': datetime_convert_timezone(
-            datetime_string_to_datetime(
-                tweet_json.get('created_at'), tweet_created_at_format
-            ), to_tz
-        ),
-        'source': tweet_json.get('source'),
-        'original_text': text,
-        'clean_text': text_cleaned,
-        'polarity': polarity,
-        'subjectivity': subjectivity,
-        'lang': tweet_json.get('lang'),
-        'favorite_count': tweet_json.get('favorite_count'),
-        'retweet_count': tweet_json.get('retweet_count'),
-        'original_author': tweet_json.get('user', {'screen_name': None}).get('screen_name'),
-        'possibly_sensitive': tweet_json.get('possibly_sensitive'),
-        'hashtags': ', '.join([
-            hashtags.get('text')
-            for hashtags in tweet_json.get('entities', {'hashtags': []}).get('hashtags', [])
-        ]),
-        'mentions': ', '.join(tweet_json.get('entities', {'user_mentions': []}).get('user_metions', [])),
-        'location': tweet_json.get('location', '')
-    }
+    def organize_tweet(self, tweet_json):
+        text = tweet_json.get('retweeted_status').get('text') if 'retweeted_status' in tweet_json else tweet_json.get('text')
+        
+        tweet_entities = tweet_json.get('entities')
+        urls = [
+            url.get('url')
+            for url in tweet_entities.get('urls', [])+
+            tweet_entities.get('media', [])+
+            tweet_json.get('extended_entities', {}).get('media', []) 
+        ] + [
+            url.get('url')
+            for url in 
+            (
+                tweet_json.get('retweeted_status')
+                .get('entities', {})
+                .get('urls', []) 
+                if tweet_json.get('retweeted_status') 
+                else []
+            )
+        ]
+        user_mentions = [
+            (user_mention.get('screen_name'), user_mention.get('name'))
+            for user_mention in tweet_entities.get('user_mentions')
+        ]
+        text_cleaned = self.clean_tweet(text, urls, user_mentions)
+        polarity, subjectivity = self.text_sentiment(text_cleaned)
+
+        return {
+            'id': tweet_json.get('id_str'),
+            'created_at': datetime_convert_timezone(
+                datetime_string_to_datetime(
+                    tweet_json.get('created_at'), self.tweet_created_at_format
+                ), self.to_tz
+            ),
+            'source': tweet_json.get('source'),
+            'original_text': text,
+            'clean_text': text_cleaned,
+            'polarity': polarity,
+            'subjectivity': subjectivity,
+            'lang': tweet_json.get('lang'),
+            'favorite_count': tweet_json.get('favorite_count'),
+            'retweet_count': tweet_json.get('retweet_count'),
+            'original_author': tweet_json.get('user', {}).get('screen_name', ''),
+            'possibly_sensitive': tweet_json.get('possibly_sensitive'),
+            'hashtags': ', '.join([
+                hashtags.get('text')
+                for hashtags in tweet_entities.get('hashtags', [])
+            ]),
+            'mentions': ', '.join([
+                mention[0]
+                for mention in user_mentions
+            ]),
+            'location': tweet_json.get('location', '')
+        }
--- src/text_preprocessing.py
+++ src/text_preprocessing.py
@@ -8,63 +8,82 @@ from textblob import TextBlob
 import re
 import string
 
-STOP_WORDS = STOP_WORDS | set(stopwords.words('english'))
+class TP:
+    def __init__(self, tokenizer=nltk.word_tokenize, stop_words=()):
+        self.tokenizer = tokenizer
+        self.STOP_WORDS = STOP_WORDS | set(stopwords.words('english')) | set(stop_words)
 
-#HappyEmoticons
-emoticons_happy = set(
-    [
-        ':-)', ':)', ';)', ':o)', ':]', ':3', ':c)', ':>', '=]', '8)', '=)', ':}',
-        ':^)', ':-D', ':D', '8-D', '8D', 'x-D', 'xD', 'X-D', 'XD', '=-D', '=D',
-        '=-3', '=3', ':-))', ":'-)", ":')", ':*', ':^*', '>:P', ':-P', ':P', 'X-P',
-        'x-p', 'xp', 'XP', ':-p', ':p', '=p', ':-b', ':b', '>:)', '>;)', '>:-)',
-        '<3'
-    ]
-)
+        #HappyEmoticons
+        self.emoticons_happy = set(
+            [
+                ':-)', ':)', ';)', ':o)', ':]', ':3', ':c)', ':>', '=]', '8)', '=)', ':}',
+                ':^)', ':-D', ':D', '8-D', '8D', 'x-D', 'xD', 'X-D', 'XD', '=-D', '=D',
+                '=-3', '=3', ':-))', ":'-)", ":')", ':*', ':^*', '>:P', ':-P', ':P', 'X-P',
+                'x-p', 'xp', 'XP', ':-p', ':p', '=p', ':-b', ':b', '>:)', '>;)', '>:-)',
+                '<3'
+            ]
+        )
 
-# Sad Emoticons
-emoticons_sad = set(
-    [
-        ':L', ':-/', '>:/', ':S', '>:[', ':@', ':-(', ':[', ':-||', '=L', ':<',
-        ':-[', ':-<', '=\\', '=/', '>:(', ':(', '>.<', ":'-(", ":'(", ':\\', ':-c',
-        ':c', ':{', '>:\\', ';('
-    ]
-)
+        # Sad Emoticons
+        self.emoticons_sad = set(
+            [
+                ':L', ':-/', '>:/', ':S', '>:[', ':@', ':-(', ':[', ':-||', '=L', ':<',
+                ':-[', ':-<', '=\\', '=/', '>:(', ':(', '>.<', ":'-(", ":'(", ':\\', ':-c',
+                ':c', ':{', '>:\\', ';('
+            ]
+        )
 
-emoticons = emoticons_happy.union(emoticons_sad)
+        self.emoticons = self.emoticons_happy.union(self.emoticons_sad)
 
-#Emoji patterns
-emoji_pattern = re.compile(
-    "["
-    u"\U0001F600-\U0001F64F"  # emoticons
-    u"\U0001F300-\U0001F5FF"  # symbols & pictographs
-    u"\U0001F680-\U0001F6FF"  # transport & map symbols
-    u"\U0001F1E0-\U0001F1FF"  # flags (iOS)
-    u"\U00002702-\U000027B0"
-    u"\U000024C2-\U0001F251"
-    "]+", 
-    flags=re.UNICODE
-)
+        #Emoji patterns
+        self.emoji_pattern = re.compile(
+            "["
+            u"\U0001F600-\U0001F64F"  # emoticons
+            u"\U0001F300-\U0001F5FF"  # symbols & pictographs
+            u"\U0001F680-\U0001F6FF"  # transport & map symbols
+            u"\U0001F1E0-\U0001F1FF"  # flags (iOS)
+            u"\U00002702-\U000027B0"
+            u"\U000024C2-\U0001F251"
+            "]+", 
+            flags=re.UNICODE
+        )
 
-def clean_tweet(tweet, urls):
-    for url in urls:
-        tweet = tweet.replace(url, '')
+        #string.punctuation
+        punctuation = list(string.punctuation)
+        punctuation.remove('$')
+        self.punctuation_pattern = re.compile(
+            r'['+''.join(punctuation)+']'
+        )
+
+    def clean_tweet(self, tweet, urls=[], user_mentions=[]):
+        for entities in urls:
+            if not entities: continue
+            tweet = tweet.replace(entities, '')
+        
+        for entities in user_mentions:
+            if not entities: continue
+            tweet = tweet.replace(*entities)
+        
+        tweet = re.sub(r':', '', tweet)
+        tweet = re.sub(r'‚Ä¶', '', tweet)
+        #replace consecutive non-ASCII characters with a space
+        tweet = re.sub(r'[^\x00-\x7F]+',' ', tweet)
+
+        #remove emojis from tweet
+        tweet = self.emoji_pattern.sub(r'', tweet)
+
+        #remove punctuation from tweet
+        tweet = self.punctuation_pattern.sub(r'', tweet)
+
+        return ' '.join(
+            [
+                w 
+                for w in self.tokenizer(tweet) 
+                if not w in self.STOP_WORDS and w not in self.emoticons
+            ]
+        )
+
+    def text_sentiment(self, text):
+        text_sentiment = TextBlob(text).sentiment
+        return text_sentiment.polarity, text_sentiment.subjectivity
     
-    tweet = re.sub(r':', '', tweet)
-    tweet = re.sub(r'‚Ä¶', '', tweet)
-    #replace consecutive non-ASCII characters with a space
-    tweet = re.sub(r'[^\x00-\x7F]+',' ', tweet)
-
-    #remove emojis from tweet
-    tweet = emoji_pattern.sub(r'', tweet)
-
-    return ' '.join(
-        [
-            w 
-            for w in nltk.word_tokenize(tweet) 
-            if not w in STOP_WORDS and w not in emoticons and w not in string.punctuation
-        ]
-    )
-
-def text_sentiment(text):
-    text_sentiment = TextBlob(text).sentiment
-    return text_sentiment.polarity, text_sentiment.subjectivity
